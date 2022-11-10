886236d080f8eec22b9dfe66252389056418b2ed
jn_xyp <jinan.xyp@gmail.com>
2022-11-10 14:08:59 -0500

    Add folders

--- hearing-app/src/App.js
+++ hearing-app/src/App.js
@@ -1,17 +1,20 @@
 import logo from './logo.svg';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          牛蛙，您run起来了!
-        </p>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    牛蛙，您run起来了!
+                </p>
+            </header>
+
+        <Footer/>
+        </div>
+    );
 }
 
 export default App;
---
+++ hearing-app/src/components/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer className="App-footer">
+            <p> Copyright © 2022 Hearing Test App  </p>
+        </footer>
+    );
+}
+
+export default Footer;
