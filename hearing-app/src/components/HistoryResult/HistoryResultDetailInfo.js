0f6a53dbcfe82068dee344e0bf4ee447afa805bd
wangxua <Xuan.Wang@pointclickcare.com>
2022-11-15 21:50:52 -0500

    add more detail to result detail page

--- hearing-app/src/components/HistoryResult/HistoryResultDetailInfo.js
+++ hearing-app/src/components/HistoryResult/HistoryResultDetailInfo.js
@@ -3,11 +3,86 @@ import "./HistoryResultDetailInfo.css";
 export function HistoryResultDetailInfo1() {
     return (
         <div>
-            <h4>Current User: Username</h4>
-            <div className="gauge1">
-                <div className="percentage"></div>
-                <div className="mask"></div>
-                <span className="value">46%</span>
+            <div>
+                <h4>Current User: Username</h4>
+                <div className="gauge1">
+                    <div className="percentage1"></div>
+                    <div className="mask1"></div>
+                    <span className="value1">46</span>
+                </div>
+                <div>Score</div>
+            </div>
+            <hr/>
+            <div className="container text-center">
+                <div className="row">
+                    <div className="col">
+                        <div> Original Score</div>
+                        <div>40</div>
+                    </div>
+                    <div className="col">
+                        <div> Score Increased</div>
+                        <div>6</div>
+                    </div>
+                    <div className="col">
+                        <div> Target Score</div>
+                        <div>50</div>
+                    </div>
+                </div>
+            </div>
+            <hr/>
+            <div className="row">
+                <div className="col-4">
+                    Your Score
+                    <div>46</div>
+                </div>
+                <div className="col-8">
+                    You achieved better score from last time, it is still worse than the score of you age group
+                </div>
+            </div>
+            <hr/>
+            <div className="container text-center">
+                <div className="row">
+                    <div className="col">
+                        <div className="card">
+                            <div className="card-header">
+                                Left ear
+                            </div>
+                            <div className="card-body">
+                                30
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card">
+                            <div className="card-header">
+                                Right ear
+                            </div>
+                            <div className="card-body">
+                                50
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card">
+                            <div className="card-header">
+                                Ear age
+                            </div>
+                            <div className="card-body">
+                                70
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card">
+                            <div className="card-header">
+                                Ear type
+                            </div>
+                            <div className="card-body">
+                                Right-preferred
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
@@ -15,12 +90,179 @@ export function HistoryResultDetailInfo1() {
 
 export function HistoryResultDetailInfo2() {
     return (
-        <h4>Current User: Username</h4>
+        <div>
+            <div>
+                <h4>Current User: Username</h4>
+                <div className="gauge2">
+                    <div className="percentage2"></div>
+                    <div className="mask2"></div>
+                    <span className="value2">73</span>
+                </div>
+                <div>Score</div>
+            </div>
+            <hr/>
+            <div className="container text-center">
+                <div className="row">
+                    <div className="col">
+                        <div> Original Score</div>
+                        <div>60</div>
+                    </div>
+                    <div className="col">
+                        <div> Score Increased</div>
+                        <div>13</div>
+                    </div>
+                    <div className="col">
+                        <div> Target Score</div>
+                        <div>75</div>
+                    </div>
+                </div>
+            </div>
+            <hr/>
+            <div className="row">
+                <div className="col-4">
+                    Your Score
+                    <div>73</div>
+                </div>
+                <div className="col-8">
+                    You achieved better score from last time, it is still worse than the score of you age group
+                </div>
+            </div>
+            <hr/>
+            <div className="container text-center">
+                <div className="row">
+                    <div className="col">
+                        <div className="card">
+                            <div className="card-header">
+                                Left ear
+                            </div>
+                            <div className="card-body">
+                                <h5 className="card-title">30</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card">
+                            <div className="card-header">
+                                Right ear
+                            </div>
+                            <div className="card-body">
+                                <h5 className="card-title">50</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card">
+                            <div className="card-header">
+                                Ear age
+                            </div>
+                            <div className="card-body">
+                                <h5 className="card-title">70</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card">
+                            <div className="card-header">
+                                Ear type
+                            </div>
+                            <div className="card-body">
+                                <h5 className="card-title">Right-preferred</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
     );
 }
 
 export function HistoryResultDetailInfo3() {
     return (
-        <h4>Current User: Username</h4>
+
+        <div>
+            <div>
+                <h4>Current User: Username</h4>
+                <div className="gauge3">
+                    <div className="percentage3"></div>
+                    <div className="mask3"></div>
+                    <span className="value3">90</span>
+                </div>
+                <div>Score</div>
+            </div>
+            <hr/>
+            <div className="container text-center">
+                <div className="row">
+                    <div className="col">
+                        <div> Original Score</div>
+                        <div>90</div>
+                    </div>
+                    <div className="col">
+                        <div> Score Increased</div>
+                        <div>0</div>
+                    </div>
+                    <div className="col">
+                        <div> Target Score</div>
+                        <div>90</div>
+                    </div>
+                </div>
+            </div>
+            <hr/>
+            <div className="row">
+                <div className="col-4">
+                    Your Score
+                    <div>90</div>
+                </div>
+                <div className="col-8">
+                    You achieved the same score from last time, it is the average score from your age group. Nice job!
+                </div>
+            </div>
+            <hr/>
+            <div className="container text-center">
+                <div className="row">
+                    <div className="col">
+                        <div className="card">
+                            <div className="card-header">
+                                Left ear
+                            </div>
+                            <div className="card-body">
+                                <h5 className="card-title">30</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card">
+                            <div className="card-header">
+                                Right ear
+                            </div>
+                            <div className="card-body">
+                                <h5 className="card-title">50</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card">
+                            <div className="card-header">
+                                Ear age
+                            </div>
+                            <div className="card-body">
+                                <h5 className="card-title">70</h5>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card">
+                            <div className="card-header">
+                                Ear type
+                            </div>
+                            <div className="card-body">
+                                <h5 className="card-title">Right-preferred</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
 }
