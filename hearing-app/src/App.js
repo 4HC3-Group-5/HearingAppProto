3175da77b94bb5ba7ed143871c5571de20db2edd
yukizyx <yukizyx230@gmail.com>
2022-11-15 22:07:52 -0500

    Implement basic grid layout of home page

--- hearing-app/src/App.js
+++ hearing-app/src/App.js
@@ -2,11 +2,15 @@ import logo from "./logo.svg";
 import Footer from "./components/Footer";
 import "./App.css";
 import Header from "./components/Header";
+import HomePage from "./components/HomePage";
 
 function App() {
   return (
     <div className="App">
       <Header />
+
+      <HomePage />
+
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>牛蛙，您run起来了!</p>
--- hearing-app/src/components/Footer.js
+++ hearing-app/src/components/Footer.js
@@ -1,19 +1,26 @@
 function Footer() {
-    return (
-        <div className="footer" >
-            <footer className="py-3 bg-light fixed-bottom">
-                <div className="container">
-                    <div className="m-0 text-center">
-                        Copyright © 2022 Hearing Test App. Hamilton, Canada. All rights reserved.
-                    </div>
-                    <a className="m-2" href="#"><img src="https://img.icons8.com/android/24/000000/facebook-new.png"></img></a>
+  return (
+    <div className="footer">
+      <footer className="py-3 bg-light">
+        <div className="container">
+          <div className="m-0 text-center">
+            Copyright © 2022 Hearing Test App. Hamilton, Canada. All rights
+            reserved.
+          </div>
+          <a className="m-2" href="#">
+            <img src="https://img.icons8.com/android/24/000000/facebook-new.png"></img>
+          </a>
 
-                    <a className="m-2" href="#"><img src="https://img.icons8.com/android/24/000000/twitter.png"></img></a>
-                    <a className="m-2" href="#">Privacy Policy</a>
-                </div>
-            </footer>
+          <a className="m-2" href="#">
+            <img src="https://img.icons8.com/android/24/000000/twitter.png"></img>
+          </a>
+          <a className="m-2" href="#">
+            Privacy Policy
+          </a>
         </div>
-    );
+      </footer>
+    </div>
+  );
 }
 
 export default Footer;
