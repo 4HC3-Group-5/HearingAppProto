23890b2385282aa3f0213b010e9dba9177a5527c
yukizyx <yukizyx230@gmail.com>
2022-11-16 12:52:49 -0500

    Add home page css

--- hearing-app/src/components/HomePage.js
+++ hearing-app/src/components/HomePage.js
@@ -4,14 +4,41 @@ import "./HomePage.css";
 function MainPage() {
   return (
     <div className="main-menu">
-      <button className="box1">Hearing Q&As</button>
-
-      <button className="box2">
-        <i class="fa-solid fa-headphones"></i>Taking a test
+      <button className="box1 btn-gradient btn-info">
+        <div className="btn-container">
+          <div className="icon-container">
+            <span class="material-symbols-outlined info-icon">info</span>
+          </div>
+          <div className="text-container">Hearing Q&As</div>
+        </div>
       </button>
 
-      <button className="box3">Sign-up</button>
-      <button className="box4">Log-in</button>
+      <button className="box2 btn-gradient btn-test">
+        <div className="btn-container">
+          <div className="icon-container">
+            <span class="material-symbols-outlined test-icon">hearing</span>
+          </div>
+          <div className="text-container">Taking a test</div>
+        </div>
+      </button>
+
+      <button className="box3 btn-gradient btn-user">
+        <div className="btn-container">
+          <div className="icon-container">
+            <span class="material-symbols-outlined user-icon">person_add</span>
+          </div>
+          <div className="text-container user-text">Sign-up</div>
+        </div>
+      </button>
+
+      <button className="box4 btn-gradient btn-user">
+        <div className="btn-container">
+          <div className="icon-container">
+            <span class="material-symbols-outlined user-icon">person</span>
+          </div>
+          <div className="text-container user-text">Log-in</div>
+        </div>
+      </button>
     </div>
   );
 }
