a609ae3f06b028ecb8d0f1f6f5e29c52a168703f
HideOnHeart0507 <hishonorneverfail@gmail.com>
2022-11-20 20:16:13 -0500

    added portals

--- hearing-app/src/components/HearingTest/PureTone.js
+++ hearing-app/src/components/HearingTest/PureTone.js
@@ -6,12 +6,12 @@ import { Link, NavLink, Outlet } from "react-router-dom";
 export function PureToneCheck() {
   return (
     <div className="parent">
-      <div className="div4 btn-gradient btn-start">
+      <Link to="../puretone-test1" className="div4 btn-gradient btn-start">
         <div className="icon-container">
           <span class="material-symbols-outlined icon">play_arrow</span>
         </div>
         <div className="text-container start-text">Start!</div>
-      </div>
+      </Link>
 
       <div className="div1">
         <div class="stepper-wrapper">
@@ -35,7 +35,7 @@ export function PureToneCheck() {
       </div>
 
       <div className="check2">
-        <Link to="../puretone-test" className="btn-gradient btn-check1">
+        <Link to="../puretone-check" className="btn-gradient btn-check1">
           <div className="btn-container1">
             <div className="icon-container">
               <span class="material-symbols-outlined icon">headphones</span>
@@ -51,7 +51,7 @@ export function PureToneCheck() {
       </div>
 
       <div className="check3">
-        <Link to="../masked" className="btn-gradient btn-check2">
+        <Link to="../puretone-check" className="btn-gradient btn-check2">
           <div className="btn-container1">
             <div className="icon-container">
               <span class="material-symbols-outlined icon">laptop_windows</span>
@@ -99,7 +99,7 @@ export function PureToneCheck() {
   );
 }
 
-export function PureToneTest() {
+export function PureToneTest1() {
   return (
     <div className="test1-parent">
       <div className="test1-div1">
@@ -144,9 +144,207 @@ export function PureToneTest() {
           <div className="hold-container">HOLD!</div>
         </button>
 
-        <button className="box2 btn-gradient btn-next">
+        <Link to="../puretone-test2" className="box2 btn-gradient btn-next">
           <div className="hold-container">Next...</div>
+        </Link>
+      </div>
+    </div>
+
+    // <h3>Instructions</h3>
+    // <p id="Instructions">This is the Pure Tone Test.</p>
+    // <div>
+    //   <Link to={`/check`} className="box2 btn-gradient btn-test">
+    //     <div className="btn-container">
+    //       <div className="icon-container">
+    //         <span class="material-symbols-outlined test-icon">hearing</span>
+    //       </div>
+    //       <div className="text-container">Something will happen!</div>
+    //     </div>
+    //   </Link>
+    // </div>
+  );
+}
+
+export function PureToneTest2() {
+  return (
+    <div className="test1-parent">
+      <div className="test1-div1">
+        <div class="stepper-wrapper">
+          <div class="stepper-item completed">
+            <div class="step-counter">1</div>
+            <div class="step-name">Select a Test</div>
+          </div>
+          <div class="stepper-item completed">
+            <div class="step-counter">2</div>
+            <div class="step-name">Check Environment</div>
+          </div>
+          <div class="stepper-item active">
+            <div class="step-counter">3</div>
+            <div class="step-name">Complete Test</div>
+          </div>
+          <div class="stepper-item">
+            <div class="step-counter">4</div>
+            <div class="step-name">See Result</div>
+          </div>
+        </div>
+      </div>
+
+      <div className="test1-div2">
+        <div class="progress">
+          <div
+            class="progress-bar progress-bar-striped bg-success w-25"
+            role="progressbar"
+            aria-valuenow="25"
+            aria-valuemin="0"
+            aria-valuemax="100"
+          ></div>
+        </div>
+      </div>
+
+      <div className="test1-div3 test-ins">
+        Hold the button when you can hear...
+      </div>
+
+      <div className="test1-div4">
+        <button className="box2 btn-gradient btn-hold">
+          <div className="hold-container">HOLD!</div>
         </button>
+
+        <Link to="../puretone-test3" className="box2 btn-gradient btn-next">
+          <div className="hold-container">Next...</div>
+        </Link>
+      </div>
+    </div>
+
+    // <h3>Instructions</h3>
+    // <p id="Instructions">This is the Pure Tone Test.</p>
+    // <div>
+    //   <Link to={`/check`} className="box2 btn-gradient btn-test">
+    //     <div className="btn-container">
+    //       <div className="icon-container">
+    //         <span class="material-symbols-outlined test-icon">hearing</span>
+    //       </div>
+    //       <div className="text-container">Something will happen!</div>
+    //     </div>
+    //   </Link>
+    // </div>
+  );
+}
+
+export function PureToneTest3() {
+  return (
+    <div className="test1-parent">
+      <div className="test1-div1">
+        <div class="stepper-wrapper">
+          <div class="stepper-item completed">
+            <div class="step-counter">1</div>
+            <div class="step-name">Select a Test</div>
+          </div>
+          <div class="stepper-item completed">
+            <div class="step-counter">2</div>
+            <div class="step-name">Check Environment</div>
+          </div>
+          <div class="stepper-item active">
+            <div class="step-counter">3</div>
+            <div class="step-name">Complete Test</div>
+          </div>
+          <div class="stepper-item">
+            <div class="step-counter">4</div>
+            <div class="step-name">See Result</div>
+          </div>
+        </div>
+      </div>
+
+      <div className="test1-div2">
+        <div class="progress">
+          <div
+            class="progress-bar progress-bar-striped bg-success w-25"
+            role="progressbar"
+            aria-valuenow="25"
+            aria-valuemin="0"
+            aria-valuemax="100"
+          ></div>
+        </div>
+      </div>
+
+      <div className="test1-div3 test-ins">
+        Hold the button when you can hear...
+      </div>
+
+      <div className="test1-div4">
+        <button className="box2 btn-gradient btn-hold">
+          <div className="hold-container">HOLD!</div>
+        </button>
+
+        <Link to="../puretone-test4" className="box2 btn-gradient btn-next">
+          <div className="hold-container">Next...</div>
+        </Link>
+      </div>
+    </div>
+
+    // <h3>Instructions</h3>
+    // <p id="Instructions">This is the Pure Tone Test.</p>
+    // <div>
+    //   <Link to={`/check`} className="box2 btn-gradient btn-test">
+    //     <div className="btn-container">
+    //       <div className="icon-container">
+    //         <span class="material-symbols-outlined test-icon">hearing</span>
+    //       </div>
+    //       <div className="text-container">Something will happen!</div>
+    //     </div>
+    //   </Link>
+    // </div>
+  );
+}
+
+export function PureToneTest4() {
+  return (
+    <div className="test1-parent">
+      <div className="test1-div1">
+        <div class="stepper-wrapper">
+          <div class="stepper-item completed">
+            <div class="step-counter">1</div>
+            <div class="step-name">Select a Test</div>
+          </div>
+          <div class="stepper-item completed">
+            <div class="step-counter">2</div>
+            <div class="step-name">Check Environment</div>
+          </div>
+          <div class="stepper-item active">
+            <div class="step-counter">3</div>
+            <div class="step-name">Complete Test</div>
+          </div>
+          <div class="stepper-item">
+            <div class="step-counter">4</div>
+            <div class="step-name">See Result</div>
+          </div>
+        </div>
+      </div>
+
+      <div className="test1-div2">
+        <div class="progress">
+          <div
+            class="progress-bar progress-bar-striped bg-success w-25"
+            role="progressbar"
+            aria-valuenow="25"
+            aria-valuemin="0"
+            aria-valuemax="100"
+          ></div>
+        </div>
+      </div>
+
+      <div className="test1-div3 test-ins">
+        Hold the button when you can hear...
+      </div>
+
+      <div className="test1-div4">
+        <button className="box2 btn-gradient btn-hold">
+          <div className="hold-container">HOLD!</div>
+        </button>
+
+        <Link to="../puretone-test-end" className="box2 btn-gradient btn-next">
+          <div className="hold-container">Next...</div>
+        </Link>
       </div>
     </div>
 
